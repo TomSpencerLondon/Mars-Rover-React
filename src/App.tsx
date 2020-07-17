149fb8d27e558d566a0c389e1b4ec6e80fab8463
Tom <thomas.spencer@codurance.com>
2020-07-17 19:52:44 +0100

    Add correct position for rover on grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import ContainedButtons from "./Button";
 import produce from 'immer';
-const numRows = 10;
-const numCols = 10;
+const width = 10;
+const height = 10;
 
 function App() {
+  const [rover, setRover] = useState(  {
+    position: {
+      x: 0,
+      y: 0
+    },
+    direction: "N",
+    blocked: false
+  });
+
   const [grid, setGrid] = useState(() => {
     const rows = [];
-    for (let i = 0; i < numRows; i++){
-      rows.push(Array.from(Array(numCols), () => 0));
+    for (let i = 0; i < width; i++){
+      let row = [];
+      for (let j = 0; j < height; j++){
+        if ((rover.position.x == height - i - 1) && (rover.position.y == j)){
+          row.push(1);
+        }else{
+          row.push(0);
+        }
+      }
+      rows.push(row);
     }
-
     return rows;
   });
 
@@ -19,18 +35,12 @@ function App() {
     <>
     <div style={{
       display: 'grid',
-      gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      gridTemplateColumns: `repeat(${height}, 20px)`,
       margin: 40
     }}>
       { grid.map((rows, i) =>
         rows.map((col, k) => (
           <div key={`${i}-${k}`}
-               onClick={() => {
-                 const newGrid = produce(grid, gridCopy => {
-                   gridCopy[i][k] = grid[i][k] ? 0 : 1;
-                 })
-                 setGrid(newGrid);
-               }}
                style={{
         width: 20,
         height: 20,
