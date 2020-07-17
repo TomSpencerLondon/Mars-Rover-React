1380df069ab1d97030704c495ff009279f43ea30
Tom <thomas.spencer@codurance.com>
2020-07-17 19:20:46 +0100

    Add files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ContainedButtons from "./Button";
+import produce from 'immer';
+const numRows = 10;
+const numCols = 10;
 
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++){
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      margin: 40
+    }}>
+      { grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div key={`${i}-${k}`}
+               onClick={() => {
+                 const newGrid = produce(grid, gridCopy => {
+                   gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                 })
+                 setGrid(newGrid);
+               }}
+               style={{
+        width: 20,
+        height: 20,
+        backgroundColor: grid[i][k] ? 'pink' : undefined,
+        border: 'solid 1px black'
+      }}/>
+          ))
+      )}
     </div>
+      <ContainedButtons />
+      </>
   );
 }
 
